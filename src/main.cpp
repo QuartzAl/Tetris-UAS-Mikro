627820e628f826dec6afce074c8a196a22f2fc71
QuartzAl <55610038+QuartzAl@users.noreply.github.com>
2023-11-30 22:59:24 +0700

    fix collision, proper delay, block rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 #include <LEDMatrixDriver.hpp>
-// This sketch will 'flood fill' your LED matrix using the hardware SPI driver Library by Bartosz Bielawski.
-// Example written 16.06.2017 by Marko Oette, www.oette.info
-
-// Define the ChipSelect pin for the led matrix (Dont use the SS or MISO pin of your Arduino!)
-// Other pins are Arduino specific SPI pins (MOSI=DIN, SCK=CLK of the LEDMatrix) see https://www.arduino.cc/en/Reference/SPI
 #define LEDMATRIX_CS_PIN 7
 #define TETRIS_RIGHT_PIN 2
 #define TETRIS_LEFT_PIN 3
@@ -14,210 +9,346 @@
 #define LEDMATRIX_SEGMENTS 4
 const int LEDMATRIX_WIDTH = LEDMATRIX_SEGMENTS * 8;
 
+struct coordVector {
+  int x;
+  int y;
+};
 
 // The LEDMatrixDriver class instance
 LEDMatrixDriver lmd(LEDMATRIX_SEGMENTS, LEDMATRIX_CS_PIN);
 
 // Helper function to print a byte as binary for debugging
 void printBinary(uint8_t num) {
-    // Loop for each bit position
-    for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
-        // Extract the bit at position 'i' using shift and AND operation
-        int bit = (num >> i) & 1;
-        Serial.print(bit);
-    }
-    Serial.println();
+  // Loop for each bit position
+  for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
+    // Extract the bit at position 'i' using shift and AND operation
+    int bit = (num >> i) & 1;
+    Serial.print(bit);
+  }
+  Serial.println();
 }
 
 const uint8_t blocks[][4] = {
-  {0x02,0x02,0x03,0x00}, // L
-  {0x01,0x01,0x03,0x00}, // J
-  {0x01,0x01,0x01,0x01}, // I
-  {0x03,0x03,0x00,0x00}, // O
-  {0x02,0x07,0x00,0x00}, // T
-  {0x06,0x03,0x00,0x00}, // Z
-  {0x03,0x06,0x00,0x00}  // S
+    {0x02, 0x02, 0x03, 0x00}, // L
+    {0x01, 0x01, 0x03, 0x00}, // J
+    {0x01, 0x01, 0x01, 0x01}, // I
+    {0x03, 0x03, 0x00, 0x00}, // O
+    {0x02, 0x07, 0x00, 0x00}, // T
+    {0x06, 0x03, 0x00, 0x00}, // Z
+    {0x03, 0x06, 0x00, 0x00}  // S
 };
+
+// const uint8_t blocks[][4] = {
+//     {0x0e,0x15,0x1f,0x0e}, // L
+//     {0x09,0x00,0x09,0x06}, // J
+//     {0x0c,0x0a,0x0c,0x08}, // I
+//     {0x01,0x00,0x00,0x00}, // O
+//     {0x09,0x0f,0x0f,0x06}, // T
+//     {0x00,0x08,0x0f,0x0f}, // Z
+//     {0x08,0x08,0x0e,0x0a}  // S
+// };
 
 // 32 rows of 8 bits for the main grid that is displayed
 // extra 4 rows for the top of the grid as top safe area
 // extra 1 row for the bottom of the grid as bottom safe area
-const int DISPLAY_Y_START = 4;                               //defines what y coordinate the top of the led matrix is
-const int DISPLAY_Y_END = DISPLAY_Y_START + LEDMATRIX_WIDTH; //defines what y coordinate the bottom of the led matrix is
+const int DISPLAY_Y_START =
+    4; // defines what y index the top of the led matrix is
+const int DISPLAY_Y_END =
+    DISPLAY_Y_START + LEDMATRIX_WIDTH; // defines what y index the bottom
+                                       // of the led matrix is
 const int GRID_HEIGHT = LEDMATRIX_WIDTH + DISPLAY_Y_START + 1; // 37
 
-uint8_t placedBlocks[GRID_HEIGHT] = { 
-  0x00, 0x00, 0x00, 0x00, // y = 0
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-  0x00
-};
+uint8_t placedBlocks[GRID_HEIGHT] = {
+    0x00, 0x00, 0x00, 0x00, // y = 0
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-uint8_t getBlockElement(uint8_t *block, int x, int blockIndex){
-  if (x < 0 ){
+uint8_t getBlockElement(uint8_t *block, int x, int blockIndex) {
+  if (x < 0) {
     return block[blockIndex] >> x * -1;
-  }else{
+  } else {
     return block[blockIndex] << x;
   }
 }
 
-void displayBlock(uint8_t *block, int x, int y){
-  for (int i = 0; i < 4; i++){
-    lmd.setColumn(y + i, getBlockElement(block, x, i));
+int getBlockFeetIndex(uint8_t *block) {
+  int blockFeet;
+  for (blockFeet = 3; blockFeet >= 0; blockFeet--) {
+    if (block[blockFeet])
+      break;
   }
+  return blockFeet;
 }
 
+int getLeftMostBlockIndex(uint8_t *block) {
+  uint8_t mask = 0x08;
+  int smallestShiftBy = 5;
+  for (int rowIndex = 0; rowIndex <= 3; rowIndex++) {
+    for (int shiftBy = 0; shiftBy <= 3; shiftBy++) {
+      if ((block[rowIndex] & (mask >> shiftBy)) > 0 &&
+          shiftBy < smallestShiftBy) {
+        smallestShiftBy = shiftBy;
+        break;
+      }
+    }
+  }
+  return 3 - smallestShiftBy;
+}
 
-
-void displayBlockAndGrid(uint8_t *block, int x, int y){
+void displayBlockAndGrid(uint8_t *block, int x, int y) {
   lmd.clear();
-
   int blockIndex = 0;
-  for (int displayGrid = 0; displayGrid < LEDMATRIX_WIDTH; displayGrid++){
+
+  if (y < DISPLAY_Y_START) {
+    blockIndex = DISPLAY_Y_START - y;
+  }
+
+  for (int displayGrid = 0; displayGrid < LEDMATRIX_WIDTH; displayGrid++) {
     int indexGrid = displayGrid + DISPLAY_Y_START;
     uint8_t newRow = placedBlocks[indexGrid];
 
-    if (indexGrid >= y && indexGrid < y + 4){
-        newRow = newRow | getBlockElement(block, x, blockIndex++);
+    if (indexGrid >= y && indexGrid < y + 4) {
+      newRow = newRow | getBlockElement(block, x, blockIndex++);
     }
     lmd.setColumn(displayGrid, newRow);
   }
   lmd.display();
 }
 
-int getBlockFeetIndex(uint8_t *block){
-  int blockFeet;
-  for (blockFeet = 3; blockFeet >= 0; blockFeet--){
-    if (block[blockFeet]) break;
+bool willLand(uint8_t *block, int x, int y) {
+  int blockIndex = 0;
+  int nextBlockFeetIndex = getBlockFeetIndex(block) + y + 1;
+
+  // check if block will hit grid
+  for (int nextRowGrid = y + 1; nextRowGrid <= nextBlockFeetIndex;
+       nextRowGrid++) {
+    uint8_t blockRow = getBlockElement(block, x, blockIndex++);
+    uint8_t nextGridRow = placedBlocks[nextRowGrid];
+
+    if ((blockRow & nextGridRow) > 0) {
+      Serial.println("Block will hit grid");
+      return true;
+    }
   }
-  return blockFeet;
-}
 
-bool willLand(uint8_t *block, int x, int y){
-
-  int blockFeetIndex = getBlockFeetIndex(block);
-  uint8_t blockFeetShifted = getBlockElement(block, x, blockFeetIndex);
-  bool hasHit = false; bool hasTouchedFloor = false;
-  int rowGridCheck = y + blockFeetIndex + 1;
-
-  // check if block will hit another block
-  // TODO: fix collision system some blocks can phase through part of blocks
-  if ((blockFeetShifted & placedBlocks[rowGridCheck]) > 0){
-    Serial.println("Block will hit another block");
-    hasHit = true;
-  }
+  int blockFeetGridCoords = y + getBlockFeetIndex(block) + 1;
 
   // check if block will hit the floor
-  if ((rowGridCheck) >= DISPLAY_Y_END){
+  if ((blockFeetGridCoords) >= DISPLAY_Y_END) {
     Serial.println("Block will hit the floor");
-    hasTouchedFloor = true;
+    return true;
   }
   // block has touched element in grid or hit the floor
-  return hasHit || hasTouchedFloor;
+  return false;
 }
 
-bool addBlockToGrid(uint8_t *block, int x, int y){
-  int absoluteBlockFeetIndex = y + getBlockFeetIndex(block); 
-  if (absoluteBlockFeetIndex > GRID_HEIGHT){
+bool addBlockToGrid(uint8_t *block, int x, int y) {
+  int gridBlockFeetIndex = y + getBlockFeetIndex(block);
+  if (gridBlockFeetIndex > DISPLAY_Y_END) {
     Serial.println("Block is out of bounds");
     return false;
   }
 
   int i = 0;
   Serial.println("Adding block to grid");
-  for (int rowGrid = y; rowGrid <= absoluteBlockFeetIndex; rowGrid++){
-    placedBlocks[rowGrid] = placedBlocks[rowGrid] | getBlockElement(block, x, i);
-    printBinary(getBlockElement(block, x, i++));
+  for (int rowGrid = y; rowGrid <= gridBlockFeetIndex; rowGrid++) {
+    placedBlocks[rowGrid] =
+        placedBlocks[rowGrid] | getBlockElement(block, x, i++);
+    printBinary(placedBlocks[rowGrid]);
   }
   return true;
 }
 
-bool isValidPosition(uint8_t *block, int nextX, int nextY){
-  // TODO: Check collision with side walls 
-
-
-  // TODO: check collision with existing placed blocks
+bool isValidXPosition(uint8_t *block, int nextX, int y) {
+  // check right side
+  if (nextX < 0) {
+    Serial.println("Block hit right wall");
+    return false;
+  }
+  // check left side
+  int nextLeftSideGridIndex = getLeftMostBlockIndex(block) + nextX;
+  if (nextLeftSideGridIndex > 7) {
+    Serial.println("Block hit left wall");
+    return false;
+  }
+  // check blcok collision
+  int blockRowIndex = 0;
+  for (int rowGridIndex = y; rowGridIndex <= y + getBlockFeetIndex(block);
+       rowGridIndex++) {
+    uint8_t blockRow = getBlockElement(block, nextX, blockRowIndex++);
+    uint8_t gridRow = placedBlocks[rowGridIndex];
+    if ((blockRow & gridRow) > 0) {
+      Serial.println("Block hit existing block");
+      return false;
+    }
+  }
   return true;
 }
 
+void rotateBlock(uint8_t *block) {
+  coordVector points[4] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+  int pointIndex = 0;
+  for (int rowIndex = 0; rowIndex <= 3; rowIndex++) {
+    uint8_t blockRow = block[rowIndex];
+    for (int shiftBy = 0; shiftBy <= 3; shiftBy++) {
+      if ((blockRow & (0x01 << shiftBy)) > 0) {
+        points[pointIndex++] = {shiftBy, rowIndex};
+      }
+    }
+  }
+  // rotate points by  90 degrees
+  for (int i = 0; i < 4; i++) {
+    points[i] = {-points[i].y, points[i].x};
+  }
 
+  // shift all points from negative to positive
+  // find smallest x and y
+  int smallestX = 0;
+  int smallestY = 0;
+  for (int i = 0; i < 4; i++) {
+    if (points[i].x < smallestX) {
+      smallestX = points[i].x;
+    }
+    if (points[i].y < smallestY) {
+      smallestY = points[i].y;
+    }
+  }
+  // add by smallest to make all points positive
+  for (int i = 0; i < 4; i++) {
+    points[i].x += abs(smallestX);
+    points[i].y += abs(smallestY);
+  }
+  // empty current block
+  for (int i = 0; i < 4; i++) {
+    block[i] = 0x00;
+  }
+  // copy points to block in uint8_t form
+  for (int i = 0; i < 4; i++) {
+    block[points[i].y] |= 0x01 << points[i].x;
+  }
+}
 
-void printBlock(uint8_t *block){
-  for (int i = 0; i < 4; i++){
+void printBlock(uint8_t *block) {
+  for (int i = 0; i < 4; i++) {
     printBinary(block[i]);
   }
 }
 
-void printGrid(){
-  for (int i = 0; i < GRID_HEIGHT; i++){
+void printGrid() {
+  for (int i = 0; i < GRID_HEIGHT; i++) {
     printBinary(placedBlocks[i]);
   }
 }
 
-int x; int y; 
+int x;
+int y;
 uint8_t currentFallingBlock[4];
 
-void resetblock(){
-  x = 3; y = 0;
+void resetblock() {
+  x = 3;
+
   memcpy(currentFallingBlock, blocks[random(7)], 4);
+  y = DISPLAY_Y_START - getBlockFeetIndex(currentFallingBlock) - 1;
 }
 
-void resetGrid(){
-  for (int i = 0; i < GRID_HEIGHT; i++){
+void resetGrid() {
+  for (int i = 0; i < GRID_HEIGHT; i++) {
     placedBlocks[i] = 0x00;
   }
   resetblock();
 }
 
+unsigned long last_time;
+unsigned long drop_delay;
+unsigned long last_input;
+unsigned long current_time;
+int last_left_input;
+int last_right_input;
+unsigned long last_down_input;
+int last_rotate_input;
+
 void setup() {
-	// init the display
+  // init the display
   Serial.begin(115200);
-	lmd.setEnabled(true);
-	lmd.setIntensity(0);   // 0 = low, 10 = high
+  lmd.setEnabled(true);
+  lmd.setIntensity(0); // 0 = low, 10 = high
   pinMode(TETRIS_RIGHT_PIN, INPUT_PULLUP);
   pinMode(TETRIS_LEFT_PIN, INPUT_PULLUP);
   pinMode(TETRIS_DOWN_PIN, INPUT_PULLUP);
   pinMode(TETRIS_ROTATE_PIN, INPUT_PULLUP);
   resetblock();
+  last_time = millis();
+  last_input = last_time;
+  drop_delay = 500;
+  last_left_input = HIGH;
+  last_right_input = HIGH;
+  last_down_input = 0;
+  last_rotate_input = HIGH;
 }
 
 void loop() {
+  current_time = millis();
 
-  displayBlockAndGrid(currentFallingBlock, x, ++y);
-  
-  if (digitalRead(TETRIS_RIGHT_PIN) == LOW && isValidPosition(currentFallingBlock, x+1, y)){
-    x++;
-  }
-  if (digitalRead(TETRIS_LEFT_PIN) == LOW && isValidPosition(currentFallingBlock, x-1, y)){
-    x--;
-  }
-  if (digitalRead(TETRIS_DOWN_PIN) == LOW && y < 32){
+  if (current_time - last_time >= drop_delay) {
+    last_time = current_time;
+    Serial.println(y);
+
+    if (willLand(currentFallingBlock, x, y)) {
+      Serial.println(y);
+
+      if (y < DISPLAY_Y_START) {
+        printGrid();
+        Serial.println("Game Over");
+        resetGrid();
+        return;
+      }
+      addBlockToGrid(currentFallingBlock, x, y);
+      // TODO: check if any rows are full, remove, and shift down
+
+      resetblock();
+    }
     y++;
   }
 
-  if (digitalRead(TETRIS_ROTATE_PIN) == LOW){
-    // TODO: implement rotation
-    Serial.println("Rotate");
-  }
-
-  // check if next position will land
-  if (willLand(currentFallingBlock, x, y)){
-    Serial.println(y);
-    if (y < DISPLAY_Y_START){
-      printGrid();
-      Serial.println("Game Over");
-      resetGrid();
-      return;
+  if (digitalRead(TETRIS_RIGHT_PIN) == LOW &&
+      digitalRead(TETRIS_RIGHT_PIN) != last_right_input) {
+    if (isValidXPosition(currentFallingBlock, x + 1, y)) {
+      x++;
     }
-    addBlockToGrid(currentFallingBlock, x, y);
-    // TODO: check if any rows are full, remove, and shift down
-    resetblock();
   }
-  // TODO: change speed based on level and to not use delay for responsive inputs
-  delay(100);
+  if (digitalRead(TETRIS_LEFT_PIN) == LOW &&
+      digitalRead(TETRIS_LEFT_PIN) != last_left_input) {
+    if (isValidXPosition(currentFallingBlock, x - 1, y)) {
+      x--;
+    }
+  }
+  if (digitalRead(TETRIS_DOWN_PIN) == LOW &&
+      current_time - last_down_input >= 50) {
+    if (!willLand(currentFallingBlock, x, y)) {
+      y++;
+    }
+    last_down_input = current_time;
+  }
+
+  if (digitalRead(TETRIS_ROTATE_PIN) == LOW &&
+      digitalRead(TETRIS_ROTATE_PIN) != last_rotate_input) {
+    Serial.println("Rotate");
+    uint8_t rotatedBlock[4];
+    memcpy(rotatedBlock, currentFallingBlock, 4);
+    rotateBlock(rotatedBlock);
+    printBlock(rotatedBlock);
+
+    if (isValidXPosition(rotatedBlock, x, y)) {
+      memcpy(currentFallingBlock, rotatedBlock, 4);
+    } else {
+      Serial.println("Invalid rotation");
+    }
+  }
+  displayBlockAndGrid(currentFallingBlock, x, y);
+
+  last_left_input = digitalRead(TETRIS_LEFT_PIN);
+  last_right_input = digitalRead(TETRIS_RIGHT_PIN);
+  last_rotate_input = digitalRead(TETRIS_ROTATE_PIN);
+  delay(50);
 }
-
-
-
